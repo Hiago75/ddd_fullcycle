22b8f38510e1a874f0d6bd674250f09c9fda5743
Hiago Brenha <hiagobrenha@protonmail.com>
2024-04-22 17:31:36 -0300

    feat: implement list product route

--- src/infrastructure/api/__tests__/products.e2e.spec.ts
+++ src/infrastructure/api/__tests__/products.e2e.spec.ts
@@ -33,4 +33,37 @@ describe("E2E test for customer", () => {
 
     expect(response.status).toBe(500)
   })
+
+  it("should create a product", async () => {
+    const response = await request(app)
+      .post("/product")
+      .send({
+        name: "Product 1",
+        price: 20
+      })
+    expect(response.status).toBe(201)
+
+    const response2 = await request(app)
+      .post("/product")
+      .send({
+        name: "Product 2",
+        price: 40
+      })
+    expect(response2.status).toBe(201)
+
+    const listResponse = await request(app)
+      .get("/product")
+      .send()
+
+    expect(listResponse.status).toBe(200)
+    expect(listResponse.body.products.length).toBe(2)
+
+    const product1 = listResponse.body.products[0]
+    expect(product1.name).toBe("Product 1")
+    expect(product1.price).toBe(20)
+
+    const product2 = listResponse.body.products[1]
+    expect(product2.name).toBe("Product 2")
+    expect(product2.price).toBe(40)
+  })
 })
--- src/infrastructure/api/routes/product.route.ts
+++ src/infrastructure/api/routes/product.route.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express"
 import CreateProductUseCase from "../../../usecase/product/create/create.product.usecase"
 import ProductRepository from "../../product/repository/sequelize/product.repository"
 import { ProductType } from "../../../usecase/product/create/create.product.dto"
+import ListProductUseCase from "../../../usecase/product/list/list.product.usecase"
 
 export const productRoute = express.Router()
 
@@ -18,7 +19,17 @@ productRoute.post("/", async (req: Request, res: Response) => {
     const output = await usecase.execute(productDto)
     res.status(201).send(output)
   } catch (err) {
-    console.log(err)
+    res.status(500).send(err)
+  }
+})
+
+productRoute.get("/", async (req: Request, res: Response) => {
+  const usecase = new ListProductUseCase(new ProductRepository())
+
+  try {
+    const output = await usecase.execute({})
+    res.send(output)
+  } catch (err) {
     res.status(500).send(err)
   }
 })
